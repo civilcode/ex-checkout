d5aa34ab91dc80387a8e5643d835d5a6d45e6d3d
Nicholas Henry <nicholas.henry@gmail.com>
2018-09-08 06:14:09 -0700

    Generate ExBarUi application

---
+++ ex_bar_ui/lib/ex_bar_ui.ex
@@ -0,0 +1,18 @@
+defmodule ExBarUi do
+  @moduledoc """
+  Documentation for ExBarUi.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExBarUi.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ ex_bar_ui/test/ex_bar_ui_test.exs
@@ -0,0 +1,8 @@
+defmodule ExBarUiTest do
+  use ExUnit.Case
+  doctest ExBarUi
+
+  test "greets the world" do
+    assert ExBarUi.hello() == :world
+  end
+end
